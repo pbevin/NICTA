a20e5b47a3e5411c2e4ceb7734324371ea332b92
Pete Bevin <pete@hover.com>
2015-05-05 11:59:02 -0400

    Bind

--- src/Course/Bind.hs
+++ src/Course/Bind.hs
@@ -68,8 +68,7 @@ infixr 1 =<<
   f (a -> b)
   -> f a
   -> f b
-(<*>) =
-  error "todo: Course.Bind#(<*>)"
+f <*> a = (<$> a) =<< f
 
 infixl 4 <*>
 
@@ -82,8 +81,7 @@ instance Bind Id where
     (a -> Id b)
     -> Id a
     -> Id b
-  (=<<) =
-    error "todo: Course.Bind (=<<)#instance Id"
+  f =<< Id a = f a
 
 -- | Binds a function on a List.
 --
@@ -94,8 +92,7 @@ instance Bind List where
     (a -> List b)
     -> List a
     -> List b
-  (=<<) =
-    error "todo: Course.Bind (=<<)#instance List"
+  (=<<) = flatMap
 
 -- | Binds a function on an Optional.
 --
@@ -106,8 +103,8 @@ instance Bind Optional where
     (a -> Optional b)
     -> Optional a
     -> Optional b
-  (=<<) =
-    error "todo: Course.Bind (=<<)#instance Optional"
+  f =<< Full a = f a
+  _ =<< Empty = Empty
 
 -- | Binds a function on the reader ((->) t).
 --
@@ -118,8 +115,7 @@ instance Bind ((->) t) where
     (a -> ((->) t b))
     -> ((->) t a)
     -> ((->) t b)
-  (=<<) =
-    error "todo: Course.Bind (=<<)#instance ((->) t)"
+  f =<< g = \t -> f (g t) t
 
 -- | Flattens a combined structure to a single structure.
 --
@@ -138,8 +134,7 @@ join ::
   Bind f =>
   f (f a)
   -> f a
-join =
-  error "todo: Course.Bind#join"
+join = (id =<<)
 
 -- | Implement a flipped version of @(=<<)@, however, use only
 -- @join@ and @(<$>)@.
@@ -168,8 +163,7 @@ infixl 1 >>=
   -> (a -> f b)
   -> a
   -> f c
-(<=<) =
-  error "todo: Course.Bind#(<=<)"
+f <=< g = \a -> f =<< g a
 
 infixr 1 <=<
 
