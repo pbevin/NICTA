67adfa93ff31db3afa49b7cbd96942c8ba8debe9
Pete Bevin <pete@hover.com>
2015-05-07 13:36:00 -0400

    Compose

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -13,27 +13,40 @@ import Course.Bind
 
 newtype Compose f g a =
   Compose (f (g a))
+  deriving (Show, Eq)
 
--- Implement a Functor instance for Compose
+-- $setup
+-- >>> import Course.Core
+-- >>> import Course.List
+-- >>> import Course.Optional
+
+-- | Implement a Functor instance for Compose
+--
+-- >>> (+1) <$> Compose (Full 1 :. Full 2 :. Nil)
+-- Compose [Full 2,Full 3]
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+  f <$> (Compose a) = Compose $ (f <$>) <$> a
 
+-- | Binds a function on Compose
+--
+-- >>> Compose (Full (+1) :. Full (+2) :. Nil) <*> Compose (Full 1 :. Full 2 :. Nil)
+-- Compose [Full 2,Full 3,Full 3,Full 4]
 instance (Apply f, Apply g) =>
   Apply (Compose f g) where
 -- Implement the (<*>) function for an Apply instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  (Compose f) <*> (Compose a) = Compose $ (<*>) <$> f <*> a
 
+-- | Insert into a Compose
+--
+-- prop> pure a == Compose (Full a :. Nil)
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
+  pure = Compose . pure . pure
 
 instance (Bind f, Bind g) =>
   Bind (Compose f g) where
 -- Implement the (=<<) function for a Bind instance for Compose
   (=<<) =
-    error "todo: Course.Compose (<<=)#instance (Compose f g)"
+    error "impossible"
